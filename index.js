3610e23ea437dc41f5a475fdaa3ab5950b8cd868
Francois Lecroart <francois@e-founders.com>
2016-01-17 17:00:01 +0100

    fix legacy rtm + give access to legacy and data store from index

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ module.exports = {
   WebClient: require('./lib/clients/web/client'),
   RtmClient: require('./lib/clients/rtm/client'),
   EVENTS: require('./lib/clients/events'),
+  LegacyRTMClient: require('./lib/clients/default/legacy-rtm'),
+  SlackMemoryDataStore: require('./lib/data-store/memory-data-store'),
 };
